b7dfed4e72f70dd7238f978603e766ddbc7ffb92
Isaque Matarazzo <devmatarazzo@gmail.com>
2025-06-30 14:04:14 -0300

    Criação de uma API para seleção de categorias

--- projeto-curso/src/components/form/Select.js
+++ projeto-curso/src/components/form/Select.js
@@ -10,6 +10,11 @@ function Select({ text, name, options, handleOnChange, value}){
                 <option>
                     Selecione uma opção
                 </option>
+                {options && options.map((option) => (
+                    <option value={option.id} key={option.id}>
+                        {option.name}
+                    </option>
+                ))}
             </select>
         </div>
     )
